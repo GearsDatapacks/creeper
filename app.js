80c5be1af08ddd22f9df21f9aa5890b17e817106
Surya Rose <surya@seriousinfinitude.com>
2018-11-24 13:31:28 +0000

    creeper v0.0.2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const character = document.getElementById('character');
+const characterMove = document.getElementById('charout');
 const grass = document.getElementById('grass');
 const soil = document.getElementById('soil');
 const body = document.getElementById('body');
@@ -10,17 +11,67 @@ const heartBbox = heart1.getBBox();
 const characterWidth = character.getBBox().width;
 const characterHeight = character.getBBox().height;
 let crouching = false;
+let facing = 'right';
 let x = window.innerWidth / 2;
 let y = 0;
+
+function setOrigin (x) {
+  character.setAttribute('transform-origin', x + characterWidth / 2,250 + y + characterHeight / 2)
+}
+
+function isTouching (elem1, elem2) {
+  const left1 = elem1.getBoundingClientRect().left;
+  const right1 = elem1.getBoundingClientRect().right;
+  const left2 = elem2.getBoundingClientRect().left;
+  const right2 = elem2.getBoundingClientRect().right;
+  const bottom1 = elem1.getBoundingClientRect().bottom;
+  const top1 = elem1.getBoundingClientRect().top;
+  const bottom2 = elem2.getBoundingClientRect().bottom;
+  const top2 = elem2.getBoundingClientRect().top;
+  
+  if (left2 > left1 && left2 < right1 && bottom2 < bottom1 && bottom2 > top1) {
+    return true;
+  }
+  
+  else if (right2 < left1 && right2 > right1 && top2 > bottom1 && top2 < top1) {
+    return true;
+  }
+  
+  else if (left1 < left2 && left1 > right2 && bottom1 > bottom2 && bottom1 < top2) {
+    return true;
+  }
+  
+  else if (right1 > left2 && right1 < right2 && top1 < bottom2 && top1 > top2) {
+    return true;
+  }
+  
+  else {
+    return false;
+  }
+}
 
 function ready () {
   html.setAttribute('ready', '')
 }
 
+function flip (element) {
+  setOrigin
+  
+  if (facing === 'left') {
+    element.setAttribute('transform', 'scale(1,1)');
+    facing = 'right'
+  }
+  
+  else if (facing === 'right') {
+    element.setAttribute('transform', 'scale(-1,1)');
+    facing = 'left'
+  }
+}
+
 function moveGuy (dx, dy) {
   x += dx;
   y += dy;
-  character.setAttribute("style", `transform: translate(${x}px,${y}px);`);
+  characterMove.setAttribute('transform', `translate(${x},${y})`);
   //console.log(x)
 }
 
@@ -59,12 +110,14 @@ function stand () {
   body.setAttribute('d', 'M0,250 l20,-25 l20,25 l-20,-25 l0,-50 m-20,25 l20,-25 l20,25 l-20,-25 m10,-15 q-7.5,10 -15 0 m0,-15 l0,3 m15,0 l0,3');
   head.setAttribute('cy', '155');
   crouching = false;
+  character.removeAttribute('crouching');
 }
 
 function crouch () {
   body.setAttribute('d', 'M0,250 l20,-15 l20,15 l-20,-15 l0,-30 m-20,25 l20,-25 l20,25 l-20,-25 m10,-15 q-7.5,10 -15 0 m0,-15 l0,3 m15,0 l0,3');
   head.setAttribute('cy', '185');
   crouching = true;
+  character.setAttribute('crouching', '');
 }
 
 window.addEventListener('keydown', function (event) {
@@ -80,10 +133,10 @@ window.addEventListener('keydown', function (event) {
     return;
   }
   
-  else if (event.keyCode === 32 && x > 319 && x < 336) {
+  else if (event.keyCode === 32 && isTouching(character, dagger)) {
     character.appendChild(dagger);
     dagger.setAttribute('up', '');
-    dagger.removeAttribute('down')
+    dagger.removeAttribute('down');
   }
   
   else if (event.keyCode === 38 && crouching === true) {    
@@ -102,20 +155,32 @@ window.addEventListener('keydown', function (event) {
   
   else if (event.keyCode === 37 && crouching === true) {
     moveGuy(-5,0);
+    if (facing === 'right') {
+      flip(character);
+    }
   }
   
   else if (event.keyCode === 39 && crouching === true) {
     moveGuy(5,0);
+    if (facing === 'left') {
+      flip(character);
+    }
   }
 
   else if (event.keyCode === 39) {
     moveGuy(10,0);
+    if (facing === 'left') {
+      flip(character);
+    }
   }
   else if (event.keyCode === 40) {
     crouch();
   }
   else if (event.keyCode === 37) {
     moveGuy(-10,0);
+    if (facing === 'right') {
+      flip(character);
+    }
   }
   else {
     return;
@@ -129,7 +194,7 @@ window.addEventListener('keyup', function (event) {
   }
 });
 
-//space 0
+//space 32
 //down arrow 40
 //right arrow 39
 //up arrow 38
